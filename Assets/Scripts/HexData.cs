ce91c3ecf6f1d31e10de04993414529367f30281
Ariel Lee <leea10@rpi.edu>
2016-04-19 18:55:56 -0400

    Finish basic hexagon grid

---
+++ Assets/Scripts/HexData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class HexData {
+	// The hexagon's outer radius, also the length of one of its sides
+	public const float outerRadius = 5f;
+
+	// The hexagon's inner radius is the outerRadius * cos(30 degrees)
+	public const float innerRadius = outerRadius * 0.866025404f;
+
+	// Coordinates of the hexagon's corners local to its center
+	public static Vector3[] corners = {
+		new Vector3(0f, 0f, outerRadius),
+		new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+		new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+		new Vector3(0f, 0f, -outerRadius),
+		new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+		new Vector3(-innerRadius, 0f, 0.5f * outerRadius)
+	};
+}
+
---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HexGrid : MonoBehaviour {
+
+	// Grid dimensions
+	public int width = 6;
+	public int height = 6;
+
+	// Prefabs
+	public HexCell cellPrefab;
+	public Text cellLabelPrefab;
+
+	// Private member variables
+	Canvas gridCanvas;
+	HexCell[] cells;
+	HexMesh hexMesh;
+
+	void Awake() {
+		gridCanvas = GetComponentInChildren<Canvas>();
+		cells = new HexCell[height * width];		
+		hexMesh = GetComponentInChildren<HexMesh> ();
+
+		for (int z = 0, i = 0; z < height; z++) {
+			for (int x = 0; x < width; x++) {
+				CreateCell (x, z, i++);
+			}
+		}	
+	}
+
+	void Start() {
+		hexMesh.Triangulate (cells);
+	}
+
+	void CreateCell(int x, int z, int i) {
+		Vector3 position;
+
+		// x coordinate has an offset on odd rows and no offset on even rows
+		position.x = x * 2f * HexData.innerRadius + (z % 2)*HexData.innerRadius;
+		position.y = 0f;
+		position.z = z * 1.5f * HexData.outerRadius;
+
+		HexCell cell = cells [i] = Instantiate<HexCell> (cellPrefab);
+		cell.transform.SetParent (this.transform, false);
+		cell.transform.localPosition = position;
+
+		Text label = Instantiate<Text> (cellLabelPrefab);
+		label.rectTransform.SetParent (gridCanvas.transform, false);
+		label.rectTransform.anchoredPosition = new Vector2 (position.x, position.z);
+		label.text = x.ToString() + "\n" + z.ToString();
+	}
+}
