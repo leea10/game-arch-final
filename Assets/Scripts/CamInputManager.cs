6de408a81f6fbd2110551b16822d0be9ff36b7c0
MarinaEspinoza <espinm3@rpi.edu>
2016-05-06 00:03:45 -0400

    New Camera Controls Implemented

--- Assets/Scripts/CamInputManager.cs
+++ Assets/Scripts/CamInputManager.cs
@@ -4,46 +4,38 @@ using System.Collections;
 public class CamInputManager : MonoBehaviour {
 
 	CameraOrbit cam;
-	enum Mode{Translate, Rotate, Zoom};
-	Mode currentMode;
 
 	// Use this for initialization
 	void Start () {
 		cam = GetComponent<CameraOrbit>();
-		currentMode = Mode.Translate;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.R))
-			currentMode = Mode.Rotate;
-		else if (Input.GetKeyDown(KeyCode.T))
-			currentMode = Mode.Translate;
 
-
-		if (Input.GetKeyDown(KeyCode.LeftArrow)){
-			if (currentMode == Mode.Translate) 
-				cam.translateHorizontal(true);
-			else if (currentMode == Mode.Rotate) 
-				cam.RotateHorizontal(true);
+		if (Input.GetKeyDown(KeyCode.A)){
+			cam.translateHorizontal(true);
+		}
+		else if (Input.GetKeyDown(KeyCode.D)){
+			cam.translateHorizontal(false);
+		}
+		else if (Input.GetKeyDown(KeyCode.W)){
+			cam.translateVertical(true);
+		}
+		else if (Input.GetKeyDown(KeyCode.S)){
+			cam.translateVertical(false);
+		}
+		else if (Input.GetKeyDown(KeyCode.LeftArrow)){
+			cam.RotateHorizontal(true);
 		}
 		else if (Input.GetKeyDown(KeyCode.RightArrow)){
-			if (currentMode == Mode.Translate) 
-				cam.translateHorizontal(false);
-			else if (currentMode == Mode.Rotate) 
-				cam.RotateHorizontal(false);
+			cam.RotateHorizontal(false);
 		}
 		else if (Input.GetKeyDown(KeyCode.UpArrow)){
-			if (currentMode == Mode.Translate) 
-				cam.translateVertical(true);
-			else if (currentMode == Mode.Rotate) 
-				cam.RotateVertical(true);
-		}
+			cam.RotateVertical(true);
+		}	
 		else if (Input.GetKeyDown(KeyCode.DownArrow)){
-			if (currentMode == Mode.Translate) 
-				cam.translateVertical(false);
-			else if (currentMode == Mode.Rotate) 
-				cam.RotateVertical(false);
+			cam.RotateVertical(false);
 		}
 	}
 }
--- Assets/Scripts/CameraOrbit.cs
+++ Assets/Scripts/CameraOrbit.cs
@@ -5,22 +5,24 @@ public class CameraOrbit : MonoBehaviour {
 
 	public Transform target;
 
+
 	public float rotateIncr = 15f;
 	public float translIncr = 45f;
 
 	public void translateHorizontal(bool left){
 		float dir = -1;
 		if (!left) dir *= -1;
-		transform.Translate(translIncr*dir,0,0);
+		transform.parent.Translate(translIncr*dir,0,0);
 	}
 
 	public void translateVertical(bool up){
 		float dir = 1;
 		if (!up) dir *= -1;
-		transform.Translate(0,0,translIncr*dir);
+		transform.parent.Translate(0,0,translIncr*dir);
 
 	}
 
+     
 	public void RotateHorizontal(bool left){
 		float dir = 1;
 		if (!left) dir *= -1;
@@ -32,4 +34,6 @@ public class CameraOrbit : MonoBehaviour {
 		if (!up) dir *= -1;
 		transform.Rotate(Vector3.right, rotateIncr * dir);
 	}
+
+
 }
